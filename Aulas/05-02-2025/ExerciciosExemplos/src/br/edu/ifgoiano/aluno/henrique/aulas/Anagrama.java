13e63417910555fc27bb731978d8c5751a675f74
Henrique Gabriel <henriquegb152019@gmail.com>
2025-02-06 15:29:07 -0300

    Exemplo feito

--- Aulas/05-02-2025/ExerciciosExemplos/src/br/edu/ifgoiano/aluno/henrique/aulas/Anagrama.java
+++ Aulas/05-02-2025/ExerciciosExemplos/src/br/edu/ifgoiano/aluno/henrique/aulas/Anagrama.java
@@ -2,6 +2,34 @@ package br.edu.ifgoiano.aluno.henrique.aulas;
 
 public class Anagrama {
     public static void main(String[] args) {
+        String substantivo = "gato";
+        resolverAnagrama(substantivo.toCharArray(), 4);
 
     }
+
+    public static void rotacionarAnagrama(char[] palavra, int novoTamanho) {
+        int i;
+        int posicao = palavra.length - novoTamanho;
+        char temp = palavra[posicao];
+        for (i = posicao + 1; i < palavra.length; i++) {
+            palavra[i - 1] = palavra[i];
+        }
+        palavra[i - 1] = temp;
+    }
+
+    public static void resolverAnagrama(char [] palavra, int novoTamanho) {
+        if (novoTamanho == 1){
+            return;
+        }
+        for (int i = 0; i < novoTamanho; i++){
+            resolverAnagrama(palavra, novoTamanho - 1);
+            if (novoTamanho == palavra.length - 1) {
+                System.out.println( new String(palavra));
+            }
+            rotacionarAnagrama(palavra, novoTamanho);
+        }
+    }
+
+
+
 }

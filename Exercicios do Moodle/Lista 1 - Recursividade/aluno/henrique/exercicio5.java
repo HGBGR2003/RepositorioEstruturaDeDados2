3490001acf80ba277fa6f4391e5fa93b23994cd1
Henrique Gabriel <henriquegb152019@gmail.com>
2025-02-11 15:38:07 -0300

    Atualizando

--- Exercicios do Moodle/Lista 1 - Recursividade/aluno/henrique/exercicio5.java
+++ Exercicios do Moodle/Lista 1 - Recursividade/aluno/henrique/exercicio5.java
@@ -2,6 +2,39 @@ package aluno.henrique;
 
 public class exercicio5 {
     public static void main(String[] args) {
-        
+        int numeroNormal = quantasVezesApareceu(10);
     }
+
+    public static int quantasVezesApareceu(int numero){
+
+        if (numero <= 10) {
+            System.out.println("Número(s) " + numero +  " não tem algarimos repetidos!");
+            return 0;
+        }
+
+        System.out.println("Numéro Original: " + numero);
+        System.out.println("----------------------------------");
+
+        int[] vetorNumero = new int [10];
+        
+        while (numero > 0) {
+            int ultimoNumero = numero % 10;
+            vetorNumero[ultimoNumero]++;
+            numero /= 10;
+        }
+
+        System.out.println("Digitos repetidos:");
+        System.out.println("-------------------------------");
+
+        for(int i = 0; i < 10; i++){
+            if (vetorNumero[i] > 1) {
+                System.out.println("O Dígito " + i + " apareceu " + vetorNumero[i] + " vezes.");
+            }
+        }
+    
+
+        return quantasVezesApareceu(numero);
+    }
+
+
 }

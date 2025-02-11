fd5b11053475c146f3b46d15c51da38cf93e4aef
Henrique Gabriel <henriquegb152019@gmail.com>
2025-02-11 14:17:09 -0300

    Atualizando

--- Exercicios do Moodle/Lista 1 - Recursividade/aluno/henrique/exercicio4.java
+++ Exercicios do Moodle/Lista 1 - Recursividade/aluno/henrique/exercicio4.java
@@ -2,11 +2,31 @@ package aluno.henrique;
 
 public class exercicio4 {
     public static void main(String[] args) {
-        
+        int numero = numeroInvertido(4321);
+        System.out.println(numero);
     }
 
-    public static int numeroInvertido(){
-        return 0;
+    public static int numeroInvertido(int numeroNormalParaInvertido){
+
+        if (numeroNormalParaInvertido < 10) {
+            return numeroNormalParaInvertido;
+        }
+
+        int ultimoNumero = numeroNormalParaInvertido % 10;
+        int restante = numeroNormalParaInvertido / 10;
+
+        int quantidadeDeDigitos = 0;
+
+        int auxiliar = restante;
+
+        while (auxiliar > 0) {
+            auxiliar = auxiliar / 10;
+            quantidadeDeDigitos++;
+        }
+
+        int parcial = (int) (ultimoNumero * Math.pow(10, quantidadeDeDigitos));
+        
+        return parcial + numeroInvertido(restante);
     }
     
 }
---
+++ Exercicios do Moodle/Lista 1 - Recursividade/aluno/henrique/exercicio5.java
@@ -0,0 +1,7 @@
+package aluno.henrique;
+
+public class exercicio5 {
+    public static void main(String[] args) {
+        
+    }
+}
